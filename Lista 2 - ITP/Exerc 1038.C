18a003e62e4091172b04c3bc9f9e06aae82ef39a
Lucas <lucasdantasferreira1@gmail.com>
2026-09-08 11:54:43 -0300

    Adiciona Listas 1, 2 e 3 de ITP

---
+++ Lista 2 - ITP/Exerc 1038.C
@@ -0,0 +1,25 @@
+/* Questão 1038 - Lanche
+
+Com base na tabela abaixo, escreva um programa que leia o código de um item e a
+quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
+
+    CODIGO | ESPECIFICACAO   | PRECO
+    -------|-----------------|----------
+      1    | Cachorro Quente | R$ 4.00
+      2    | X-Salada        | R$ 4.50
+      3    | X-Bacon         | R$ 5.00
+      4    | Torrada simples | R$ 2.00
+      5    | Refrigerante    | R$ 1.50
+
+Entrada
+O arquivo de entrada contém dois valores inteiros correspondentes ao código e à
+quantidade de um item conforme tabela acima.
+
+Saída
+O arquivo de saída deve conter a mensagem "Total: R$ " seguido pelo valor a ser
+pago, com 2 casas após o ponto decimal.
+
+Exemplos:
+    Entrada: 3 2       ->  Saída: Total: R$ 10.00
+    Entrada: 2 3       ->  Saída: Total: R$ 13.50
+    Entrada: 5 1       ->  Saída: Total: R$ 1.50   */
---
+++ Lista 2 - ITP/Exerc 2057.C
@@ -0,0 +1,11 @@
+/* Questão 2057
+
+Paulo e Pedro fizeram uma longa jornada [...]. A cada escala, tinham que ajustar seus relógios por causa do fuso horário.
+Dada a hora de saída, tempo de viagem e o fuso do destino com relação à origem, informe a hora de chegada de cada vôo no destino.
+Por exemplo, se partiram às 10 horas para uma viagem de 4 horas rumo a um destino em um fuso com uma hora a mais: 10 + 4 + 1 = 15 horas. Note que se a hora calculada for igual a 24, o programa deverá imprimir 0 (zero).
+
+Entrada
+A entrada contém 3 inteiros: S (0 ≤ S ≤ 23), T (1 ≤ T ≤ 12) e F (-5 ≤ F ≤ 5), separados por um espaço, indicando respectivamente a hora da saída, o tempo de viagem e o fuso horário do destino com relação à origem.
+
+Saída
+Imprima um inteiro que indica a hora local prevista no destino, conforme os exemplos abaixo.*/
